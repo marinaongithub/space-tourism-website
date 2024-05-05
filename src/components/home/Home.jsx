4c1287948ee3f330f2c1e95013edb768bc6fd914
marinaongithub <73482347+marinaongithub@users.noreply.github.com>
2024-05-05 12:18:32 +0100

    Updated bg color for nav-list mobile

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,17 +3,16 @@ import './home.css'
 function Home() {
   return (
     <div className="main flex ff-barlow">
-        <section className="description flex">
-            <h1>SO, YOU WANT TO TRAVEL TO 
-              <br/><br/><span className="ff-bellefair">SPACE</span></h1>
-            <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
-            outer space and not hover kind of on the edge of it. Well sit back, and relax 
-            because we’ll give you a truly out of this world experience!</p>
-        </section>
-        <button className="explore ff-bellefair">
-            EXPLORE
-        </button>
-
+      <section className="description flex">
+        <h1>SO, YOU WANT TO TRAVEL TO 
+          <br/><br/><span className="ff-bellefair">SPACE</span></h1>
+        <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
+          outer space and not hover kind of on the edge of it. Well sit back, and relax 
+          because we’ll give you a truly out of this world experience!</p>
+      </section>
+      <button className="explore ff-bellefair">
+        EXPLORE
+      </button>
     </div>
   )
 }
