58aaf049ad85210894246ead051730e3a4394b12
marinaongithub <73482347+marinaongithub@users.noreply.github.com>
2024-05-06 21:46:37 +0100

    Added design for destination moon mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Destinations from './components/destination/Destinations';
 
 function App() {
   return (
     <div className="App flex">
       <Header/>
-      <Home/>
+      {/* <Home/> */}
+      <Destinations/>
     </div>
   );
 }
---
+++ src/components/destination/Destinations.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './destinations.css'
+import planet from '../../assets/destination/image-moon.webp';
+
+const Destinations = () => {
+  return (
+    <div className="destination flex ff-barlow">
+      <section className="planet flex">
+        <h2><span>01&nbsp;&nbsp;&nbsp;</span>PICK YOUR DESTINATION</h2>
+        <figure>
+          <img className="planetImg" src={planet} alt="the moon" width="445" height="445" ></img>
+        </figure> 
+      </section>   
+      <section className="details flex">
+        <nav>
+          <ul className="planetList flex">
+            <li>MOON</li>
+            <li>MARS</li>
+            <li>EUROPA</li>
+            <li>TITAN</li>
+          </ul>  
+        </nav>
+        <h3 className="ff-bellefair">MOON</h3>
+        <p className="planetInfo">
+          See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
+          regain perspective and come back refreshed. While you’re there, take in some history 
+          by visiting the Luna 2 and Apollo 11 landing sites.
+        </p>
+        {/* <div className="break"></div> */}
+        <div className="metrics flex">
+          <div>
+            <h4 >AVG. DISTANCE</h4>
+            <p className="ff-bellefair">384,400 KM</p>
+          </div>
+          <div>
+            <h4 >EST. TRAVEL TIME</h4>
+            <p className="ff-bellefair">3 DAYS</p>
+          </div>
+        </div>
+      </section>
+    </div>    
+  )
+}
+
+export default Destinations
